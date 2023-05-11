695ac38e03170d2d3a67947502e0494f3c25969a
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-05-11 19:28:34 +0200

    Add initial banner

---
+++ components/Nav/Banner.tsx
@@ -0,0 +1,82 @@
+import React, { useRef, useEffect } from 'react';
+import { SlArrowDownCircle } from 'react-icons/sl';
+import { gsap } from 'gsap';
+import SplitType from 'split-type';
+
+const Banner = (): JSX.Element => {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const subtitleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    animateString();
+  }, []);
+
+  function animateString() {
+    // create character level splits - variables not reused
+    let _ = new SplitType(titleRef.current);
+    let __ = new SplitType(subtitleRef.current);
+
+    let characters = document.querySelectorAll('.char');
+
+    for (let i = 0; i < characters.length; i++) {
+      characters[i].classList.add('translate-y-full');
+    }
+
+    gsap.to(characters, {
+      duration: 1,
+      ease: 'power4.out',
+      stagger: 0.05,
+      translateY: '0%',
+      delay: 0.5
+    });
+  }
+
+  function handleClick() {
+    const element = document.getElementById('navRef');
+    element?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest'
+    });
+  }
+
+  return (
+    <div className="hidden md:flex flex-col justify-center items-center text-center w-full h-screen bg-banner bg-cover">
+      <div className="w-2/3 p-1 backdrop-blur-lg">
+        <h1
+          id="banner-title"
+          ref={titleRef}
+          className="text-8xl lg:text-9xl mb-10"
+          style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}
+        >
+          AGSF{' '}
+        </h1>
+        <div id="banner-subtitle" ref={subtitleRef} className="text-5xl">
+          <span
+            style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}
+          >
+            Développement web pour
+          </span>
+          <span
+            style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}
+          >
+            {' '}
+            PME et indépendants
+          </span>
+        </div>
+      </div>
+      <div className="absolute bottom-0 mb-10">
+        <div className="text-3xl flex flex-col justify-center items-center cursor-pointer">
+          Continuer
+          <SlArrowDownCircle
+            onClick={handleClick}
+            className="text-5xl animate-bounce mt-6"
+            style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,45 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Avatar from '../components/Nav/Avatar';
+import Banner from '../components/Nav/Banner';
 
 const Navbar = () => {
+  const router = useRouter();
+
+  let banner;
+  if (router.pathname === '/') {
+    banner = <Banner />;
+  } else {
+    banner = null;
+  }
+
   return (
-    <nav className="flex flex-col sm:flex-row justify-around nav py-4 px-6 items-center">
-      <Avatar />
+    <>
+      {banner}
+      <nav className="flex flex-col sm:flex-row justify-around nav py-4 px-6 items-center">
+        <Avatar />
 
-      <div className="flex items-center gap-8 mt-4 sm:mt-0">
-        <Link href="/" passHref legacyBehavior>
-          <a className="nav-link hover:text-teal-500 underline underline-offset-4">
-            Accueil
-          </a>
-        </Link>
+        <div className="flex items-center gap-8 mt-4 sm:mt-0">
+          <Link href="/" passHref legacyBehavior>
+            <a className="nav-link hover:text-teal-500 underline underline-offset-4">
+              Accueil
+            </a>
+          </Link>
 
-        <Link href="/blog" passHref legacyBehavior>
-          <a className="nav-link hover:text-teal-500 underline underline-offset-4">
-            Blog
-          </a>
-        </Link>
+          <Link href="/blog" passHref legacyBehavior>
+            <a className="nav-link hover:text-teal-500 underline underline-offset-4">
+              Blog
+            </a>
+          </Link>
 
-        <Link href="/portfolio" passHref legacyBehavior>
-          <a className="nav-link hover:text-teal-500 underline underline-offset-4">
-            Portfolio
-          </a>
-        </Link>
-      </div>
-    </nav>
+          <Link href="/portfolio" passHref legacyBehavior>
+            <a className="nav-link hover:text-teal-500 underline underline-offset-4">
+              Portfolio
+            </a>
+          </Link>
+        </div>
+      </nav>
+    </>
   );
 };
 

71d21ed836df75f3739379463e3b59ea53b8788b
Adnene Guessoum <adnen.guessoum@gmail.com>
2023-05-13 02:12:16 +0200

    fix: still same issue. remove props for path

--- components/Layout/main.tsx
+++ components/Layout/main.tsx
@@ -19,7 +19,7 @@ const MainLayout = ({ children, customMeta }: LayoutProps): JSX.Element => {
   return (
     <div className="layout min-h-screen bg-gray-50 dark:bg-ivory-black">
       <CustomHead customMeta={customMeta} router={router} />
-      <Navbar path={router.asPath} />
+      <Navbar />
       <main className="flex justify-center main pt-10 m-4">{children}</main>
       <footer className="sticky top-[100vh] flex justify-center footer p-4 border border-slate-600">
         copyleft 2023 - Adnene Guessoum
